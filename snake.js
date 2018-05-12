8dfe4866f4367f83358ab28971054df2553dbd6e
DESKTOP-K0GFFHS\Vic <victor_zherdev@epam.com>
2018-05-12 12:19:24 +0300

    add collisions

--- snake.js
+++ snake.js
@@ -1,16 +1,12 @@
 var ctx;
 var canvas;
-const DELAY = 500;
+const DELAY = 200;
 
 
 function init() {
     canvas = document.getElementById('myCanvas');
     ctx = canvas.getContext('2d');
 
-    //loadImages();
-    
-    
-   // drawGrid();
     createSnake();
     locateApple();
 
@@ -19,7 +15,7 @@ function init() {
     drawGrid();
 
     setInterval("gameCycle()", DELAY);
-}    
+}
 
 const LEFT_KEY = 37;
 const RIGHT_KEY = 39;
@@ -27,49 +23,69 @@ const UP_KEY = 38;
 const DOWN_KEY = 40;
 
 var currentDirection = "down"//down by default as start
-var snake= [];
+var snake = [];
 var head = [];
 var previousTail;
 var sizeOfCell = 20;
+var apple;
 var appleExists = false;
+
+var isAlive = true;
 //step=20?
 //160 x 90
-function gameCycle(){
-    
-    moveSnake();
+function gameCycle() {
 
-    checkSnake();
+    if (isAlive) {
+        moveSnake();
 
-    drawSnake();
-    locateApple();
-    drawGrid();
+        checkSnake();
+
+        drawSnake();
+        locateApple();
+        drawGrid();
+    }
 }
 
-function checkSnake(){
-    //todo: check coord and increment snake
+function checkSnake() {
+    for(var i=1;i<snake.length;i++){
+        if(snake[0][0]==snake[i][0] && snake[0][1]==snake[i][1]){
+            isAlive=false;
+        }
+    }
+
+    if (snake[0][0] == apple[0] && snake[0][1] == apple[1]) {
+        appleExists = false;
+        feedSnake();
+    }
 }
 
-function moveSnake(){
-   
-    var newHead = [snake[0][0],snake[0][1]];
+function feedSnake() {
+    var currTail = snake.pop();
+    var newTail = [currTail[0], currTail[1]];
+    snake.push(currTail, newTail);
+}
+
+function moveSnake() {
+
+    var newHead = [snake[0][0], snake[0][1]];
     snake.unshift(newHead);
-    if(currentDirection=="down")//j++
+    if (currentDirection == "down")//j++
     {
         snake[0][1]++;
         previousTail = snake.pop();
-     
+
     }
-    else if(currentDirection=="up")//j--
+    else if (currentDirection == "up")//j--
     {
         snake[0][1]--;
         previousTail = snake.pop();
     }
-    else if(currentDirection=="left")//i--
+    else if (currentDirection == "left")//i--
     {
         snake[0][0]--;
         previousTail = snake.pop();
     }
-    else if(currentDirection=="right")//i++
+    else if (currentDirection == "right")//i++
     {
         snake[0][0]++;
         previousTail = snake.pop();
@@ -77,29 +93,32 @@ function moveSnake(){
 }
 
 
-function createSnake(){
-    head = [3,3]
-    tail1 = [3,2]
-    tail2 = [3,1]
+function createSnake() {
+    head = [3, 3]
+    tail1 = [3, 2]
+    tail2 = [3, 1]
 
-    snake[0]=head;
-    snake[1]=tail1;
-    snake[2]=tail2;
+    snake[0] = head;
+    snake[1] = tail1;
+    snake[2] = tail2;
 }
 
-function drawSnake(){
-    ctx.fillStyle="#FFFFFF";
-   // alert(snake[0,0])
-    ctx.fillRect(snake[0][0]*sizeOfCell,snake[0][1]*sizeOfCell,sizeOfCell,sizeOfCell);
+function drawSnake() {
+    if(!isAlive)
+        ctx.fillStyle = "#FF0000";
+    if (isAlive)
+        ctx.fillStyle = "#FFFFFF";
+    // alert(snake[0,0])
+    ctx.fillRect(snake[0][0] * sizeOfCell, snake[0][1] * sizeOfCell, sizeOfCell, sizeOfCell);
 
-    for(var i=1; i<snake.length;i++)
-    {
-        ctx.fillStyle="#999999";
-        ctx.fillRect(snake[i][0]*sizeOfCell,snake[i][1]*sizeOfCell,sizeOfCell,sizeOfCell);
-     //  ctx.fillRect(snake[0][i]*sizeOfCell,snake[i][0]*sizeOfCell,sizeOfCell,sizeOfCell)
+    for (var i = 1; i < snake.length; i++) {
+        if (isAlive)
+            ctx.fillStyle = "#999999";
+        ctx.fillRect(snake[i][0] * sizeOfCell, snake[i][1] * sizeOfCell, sizeOfCell, sizeOfCell);
     }
-   
-    ctx.fillStyle="#000000";
+
+    if (isAlive)
+        ctx.fillStyle = "#000000";
     if (previousTail != undefined) ctx.fillRect(previousTail[0] * sizeOfCell, previousTail[1] * sizeOfCell, sizeOfCell, sizeOfCell)
 }
 
@@ -107,29 +126,30 @@ function locateApple() {
     if (!appleExists) {
         var newApple = getNewAppleCoordinates();
         drawApple(newApple);
+        apple = newApple;
     }
     appleExists = true;
 }
 
-function drawApple(point){
-    ctx.fillStyle="#00FF00";
-    ctx.fillRect(point[0]*sizeOfCell,point[1]*sizeOfCell,sizeOfCell,sizeOfCell);
+function drawApple(point) {
+    ctx.fillStyle = "#00FF00";
+    ctx.fillRect(point[0] * sizeOfCell, point[1] * sizeOfCell, sizeOfCell, sizeOfCell);
 }
 
-function getNewAppleCoordinates(){
+function getNewAppleCoordinates() {
     var located = false;
     while (!located) {
         var xApple = randomInteger(0, 80);
         var yApple = randomInteger(0, 45);
 
-        for(var i=0;i< snake.length;i++){
-            if(xApple==snake[i][0] && YApple==snake[i][1]){
+        for (var i = 0; i < snake.length; i++) {
+            if (xApple == snake[i][0] && YApple == snake[i][1]) {
                 break;
             }
-            located=true; 
+            located = true;
         }
     }
-    return [xApple,yApple];
+    return [xApple, yApple];
 }
 
 
@@ -137,34 +157,34 @@ function randomInteger(min, max) {
     var rand = min - 0.5 + Math.random() * (max - min + 1)
     rand = Math.round(rand);
     return rand;
-  }
+}
 
-function drawGrid(){
-    ctx.fillStyle="#505050";
-    for(var i=0;i<160;i++){
-        for(var j=0;j<160;j++){
-            ctx.fillRect(i*sizeOfCell,j*sizeOfCell, 1,canvas.height);
-            ctx.fillRect(i*sizeOfCell,j*sizeOfCell, canvas.width,1);
+function drawGrid() {
+    ctx.fillStyle = "#505050";
+    for (var i = 0; i < 160; i++) {
+        for (var j = 0; j < 160; j++) {
+            ctx.fillRect(i * sizeOfCell, j * sizeOfCell, 1, canvas.height);
+            ctx.fillRect(i * sizeOfCell, j * sizeOfCell, canvas.width, 1);
         }
     }
 }
 
 
 
-onkeydown = function(e) {
-    
+onkeydown = function (e) {
+
     var key = e.keyCode;
-    
-    if ((key == LEFT_KEY) && (currentDirection!="right")) {
-        currentDirection="left";
+
+    if ((key == LEFT_KEY) && (currentDirection != "right")) {
+        currentDirection = "left";
     }
-    else if ((key == RIGHT_KEY) && (currentDirection!="left")) {
-        currentDirection="right";
+    else if ((key == RIGHT_KEY) && (currentDirection != "left")) {
+        currentDirection = "right";
     }
-    else if ((key == DOWN_KEY) && (currentDirection!="up")) {
-        currentDirection="down";
+    else if ((key == DOWN_KEY) && (currentDirection != "up")) {
+        currentDirection = "down";
     }
-    else if ((key == UP_KEY) && (currentDirection!="down")) {
-        currentDirection="up";
+    else if ((key == UP_KEY) && (currentDirection != "down")) {
+        currentDirection = "up";
     }
 }
